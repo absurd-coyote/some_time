fefaa6c9ca39aff920e72c01b93e1c602061a745
Absurd Coyote <absurd_coyote@proton.me>
2024-09-19 19:43:14 +0200

    no ltk

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import ltk
 
-(
-    ltk.VBox(
-        ltk.HBox(
-            ltk.Text("Hello"),
-            ltk.Button(
-                "World", 
-                lambda event: 
-                    ltk.find(".ltk-button, a")
-                        .css("color", "red")
-            )
-            .css("color", "blue")
-        )
-    )
-    .appendTo(ltk.window.document.body)
-)
+print("hello")
+
+# (
+#     ltk.VBox(
+#         ltk.HBox(
+#             ltk.Text("Hello"),
+#             ltk.Button(
+#                 "World", 
+#                 lambda event: 
+#                     ltk.find(".ltk-button, a")
+#                         .css("color", "red")
+#             )
+#             .css("color", "blue")
+#         )
+#     )
+#     .appendTo(ltk.window.document.body)
+# )
