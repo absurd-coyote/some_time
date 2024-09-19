faea750a9a1916ab7d66ae7b359d382929cd4b5f
Absurd Coyote <absurd_coyote@proton.me>
2024-09-19 20:47:40 +0200

    wip: update time

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import datetime
 import time
 import asyncio
 
-while True:
-    now = datetime.datetime.now()
-    pydom["div#time"].html = str(now)
-    await asyncio.sleep(1)
+# while True:
+#     now = datetime.datetime.now()
+#     pydom["div#time"].html = str(now)
+#     await asyncio.sleep(1)
 
+from datetime import datetime
+import asyncio
+
+async def clock_forever():
+    while(True):
+        now = datetime.now()
+        Element('clock-output').write(f"{now.hour}:{now.minute:02}:{now.second:02}")
+        await asyncio.sleep(1)
+
+PyScript.loop.create_task(clock_forever())
 
 # (
 #     ltk.VBox(
