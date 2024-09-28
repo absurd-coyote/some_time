46f23a4203f81b5c2ece59b0ec911fbb5e003594
Absurd Coyote <absurd_coyote@proton.me>
2024-09-28 15:56:26 +0200

    debug additional file

--- src/main.py
+++ src/main.py
@@ -11,8 +11,7 @@ from pyscript import display
 import os
 
 print("init")
-print(os.path.dirname(os.path.realpath(__file__)))
-print(os.listdir(os.path.dirname(os.path.realpath(__file__))))
+print(os.listdir("."))
 print("done")
 
 
