97df9c56031500d0d6909285cadb33f55ef446c3
Absurd Coyote <absurd_coyote@proton.me>
2024-10-26 13:59:46 +0200

    test: done test domain

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -41,7 +41,21 @@ def test_init_get_values_from_memory_when_present(input_trigger, input_memory, o
     assert time_counter.counter_running == expected_running
     assert time_counter.start_count == expected_start_count
 
-def test_start_from_init_default_count_time(input_trigger, input_memory, output_display, output_memory):
+def test_update_time_when_counter_off_does_not_increase_time(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = False
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    with PeriodicRunner(target=time_counter.update_time, period=0.001):
+        time.sleep(2)
+
+    # Then
+    assert time_counter.counter.total_seconds() == 0
+    assert time_counter.counter_running == False
+
+def test_update_time_when_started_counts_time(input_trigger, input_memory, output_display, output_memory):
     # Given
     input_memory.available.return_value = False
     time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
@@ -55,6 +69,118 @@ def test_start_from_init_default_count_time(input_trigger, input_memory, output_
     # Then
     assert time_counter.counter.total_seconds() == pytest.approx(2, abs=1e-2)
     assert time_counter.counter_running == True
-    # assert that the start count is close to now
-    start_count_diff = time_counter.start_count - datetime.datetime.now()
-    assert start_count_diff.total_seconds() == pytest.approx(0, abs=1e-2)
+
+def test_update_time_from_previously_stopped_adds_to_it(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = True
+    input_memory.load_values.return_value = {
+        "counter": 10,
+        "counter_running": False,
+        "start_count": datetime.datetime.now().isoformat(),
+
+        }
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    with PeriodicRunner(target=time_counter.update_time, period=0.001):
+        time_counter.start()
+        time.sleep(2)
+
+    # Then
+    assert time_counter.counter.total_seconds() == pytest.approx(12, abs=1e-2)
+    assert time_counter.counter_running == True
+
+def test_update_time_from_previously_running_catch_up_lost_time(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = True
+    input_memory.load_values.return_value = {
+        "counter": 10,
+        "counter_running": True,
+        "start_count": datetime.datetime.now().isoformat(),
+
+        }
+
+    # When
+    time.sleep(2)  # loss of time
+    # Then load again the counter
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+    # Run the counter for some time
+    with PeriodicRunner(target=time_counter.update_time, period=0.001):
+        time.sleep(2)
+
+    # Then
+    # expect to get "initial counter" + "lost time" + "running time"
+    assert time_counter.counter.total_seconds() == pytest.approx(14, abs=1e-1)
+    assert time_counter.counter_running == True
+
+def test_add_time_on_stopped_counter_increase_counter(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = False
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    time_counter.add_time(15)
+
+    # Then
+    assert time_counter.counter.total_seconds() == 15*60
+    assert time_counter.counter_running == False
+
+def test_remove_time_on_stopped_counter_decrease_counter(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = False
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    time_counter.remove_time(15)
+
+    # Then
+    assert time_counter.counter.total_seconds() == -15*60
+    assert time_counter.counter_running == False
+
+def test_add_time_on_running_counter_combines_with_count(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = True
+    input_memory.load_values.return_value = {
+        "counter": 10,
+        "counter_running": True,
+        "start_count": datetime.datetime.now().isoformat(),
+
+        }
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    with PeriodicRunner(target=time_counter.update_time, period=0.001):
+        time_counter.add_time(15)
+        time.sleep(2)
+
+    # Then
+    # expect to get "initial counter" + "running time" + "add time"
+    assert time_counter.counter.total_seconds() == pytest.approx(12 + (15*60), abs=1e-1)
+    assert time_counter.counter_running == True
+
+def test_remove_time_on_running_counter_combines_with_count(input_trigger, input_memory, output_display, output_memory):
+    # Given
+    input_memory.available.return_value = True
+    input_memory.load_values.return_value = {
+        "counter": 10,
+        "counter_running": True,
+        "start_count": datetime.datetime.now().isoformat(),
+
+        }
+    time_counter = TimeCounter(input_trigger, input_memory, output_display, output_memory)
+    time_counter.init()
+
+    # When
+    with PeriodicRunner(target=time_counter.update_time, period=0.001):
+        time_counter.remove_time(15)
+        time.sleep(2)
+
+    # Then
+    # expect to get "initial counter" + "running time" + "add time"
+    assert time_counter.counter.total_seconds() == pytest.approx(12 - (15*60), abs=1e-1)
+    assert time_counter.counter_running == True
